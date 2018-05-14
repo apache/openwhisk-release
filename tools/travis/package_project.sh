a0c2b61d69131e47049f95a8f6d13df801437099
Vincent Hou <shou@us.ibm.com>
2018-05-14 11:59:59 -0400

    Add the support to create the release when the git tag is added

--- tools/travis/package_project.sh
+++ tools/travis/package_project.sh
@@ -27,22 +27,28 @@ PARENTDIR="$(dirname "$CURRENTDIR")"
 SVN_USERNAME=$2
 SVN_PASSWORD=$3
 
-source "$PARENTDIR/load_config.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD" "$PARENTDIR"
-
 "$PARENTDIR/install_dependencies.sh"
-"$PARENTDIR/download_source_code.sh" "$WORK_DIR"
-if [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$PUBLISH_STAGE" == "true" ] ; then
-    "$PARENTDIR/checkout_svn.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+
+if [ ! -z "$TRAVIS_TAG" ] ; then
+    # The git tag is named after the version to be released.
+    release_version=$TRAVIS_TAG
+    source "$PARENTDIR/load_config.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD" "$PARENTDIR" "$release_version"
+    "$PARENTDIR/move_stage_to_release.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+else
+    source "$PARENTDIR/load_config.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD" "$PARENTDIR"
+    "$PARENTDIR/download_source_code.sh" "$WORK_DIR"
+    if [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$PUBLISH_STAGE" == "true" ] ; then
+        "$PARENTDIR/checkout_svn.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+    fi
+
+    "$PARENTDIR/package_source_code.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+    "$PARENTDIR/package_binaries.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+
+    if [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$PUBLISH_STAGE" == "true" ] ; then
+        "$CURRENTDIR/import_pgp_key.sh"
+        "$PARENTDIR/sign_artifacts.sh" "$WORK_DIR"
+        "$PARENTDIR/upload_artifacts.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
+    fi
+
+    "$PARENTDIR/verify_source_code.sh" "$WORK_DIR"
 fi
-
-
-"$PARENTDIR/package_source_code.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
-"$PARENTDIR/package_binaries.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
-
-if [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$PUBLISH_STAGE" == "true" ] ; then
-    "$CURRENTDIR/import_pgp_key.sh"
-    "$PARENTDIR/sign_artifacts.sh" "$WORK_DIR"
-    "$PARENTDIR/upload_artifacts.sh" "$WORK_DIR" "$SVN_USERNAME" "$SVN_PASSWORD"
-fi
-
-"$PARENTDIR/verify_source_code.sh" "$WORK_DIR"
