cc4f9a08b7d7449c82590888a9ee32ffa408db94
Vincent Hou <shou@us.ibm.com>
2018-06-22 16:02:41 -0400

    Change the confition of exclusion from .git* to .git\*

--- tools/package_source_code.sh
+++ tools/package_source_code.sh
@@ -30,7 +30,7 @@ mkdir -p $CURRENT_VERSION_DIR
 # Remove bin and build folders
 mkdir -p $OPENWHISK_CLEANED_SOURCE_DIR
 rm -rf $OPENWHISK_CLEANED_SOURCE_DIR/*
-rsync -rtp --exclude .bin --exclude .jshintrc --exclude .pydevproject --exclude .rat-excludes --exclude .git* --exclude .travis.yml --exclude bin --exclude build --exclude specification/archive --exclude specification/diagrams --exclude tests $OPENWHISK_SOURCE_DIR/. $OPENWHISK_CLEANED_SOURCE_DIR
+rsync -rtp --exclude .bin --exclude .jshintrc --exclude .pydevproject --exclude .rat-excludes --exclude .git\* --exclude .travis.yml --exclude bin --exclude build --exclude specification/archive --exclude specification/diagrams --exclude tests $OPENWHISK_SOURCE_DIR/. $OPENWHISK_CLEANED_SOURCE_DIR
 
 for repo in $(echo $repos | sed "s/,/ /g")
 do
