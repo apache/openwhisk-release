35e6ae719b29aae02c7833cefe628ad638e07390
Alexander Klimetschek <alexander@klimetschek.de>
2020-03-03 03:04:44 -0800

    exclude wskdebug.js from executable check to support new openwhisk-wskdebug repo (#330)

--- tools/rcverify.sh
+++ tools/rcverify.sh
@@ -191,7 +191,7 @@ SC=$(eval $CMD >& /dev/null)
 validate $? 0 "$CMD"
 
 printf "scanning for executable files..."
-EXE=$(find "$DIR/$BASE" -type f ! -name "*.sh" ! -name "*.sh" ! -name "*.py" ! -name "*.php" ! -name "gradlew" ! -name "gradlew.bat" ! -name "exec" ! -name "wskadmin" ! -path "*/bin/*" -perm -001)
+EXE=$(find "$DIR/$BASE" -type f ! -name "*.sh" ! -name "*.sh" ! -name "*.py" ! -name "*.php" ! -name "gradlew" ! -name "gradlew.bat" ! -name "exec" ! -name "wskadmin" ! -name "wskdebug.js" ! -path "*/bin/*" -perm -001)
 validate "$EXE" "" "$EXE"
 
 printf "scanning for unexpected file types..."
